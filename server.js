0e68ad03b9b9ca580449d75d864771d93574cdc2
mohammadkh24 <mohammadgithub2024@gmail.com>
2024-12-23 01:03:29 +0330

    add Error Handling and Not Found page and try,catch for controllers

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((err,req,res,next) => {
     statusCode : err.status || 500,
     message : err.message || "Server Error!"
   })
-})
+});
 
 // Server Listening
 app.listen(config.port, () => {
